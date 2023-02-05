b8e20ee1095295e88196b74067a17a7dbd3f94ba
Neto test <samarokova.a.g@bk.ru>
2023-02-05 17:46:32 +0300

    Init repository

---
+++ src/test/java/test.java
@@ -0,0 +1,20 @@
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+
+public class test {
+    @Test
+    void shouldSendReRequestBody() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("22.02.1997")
+
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+}
